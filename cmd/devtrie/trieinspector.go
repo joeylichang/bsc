b651f09a5ce46d70cbe9436c127ebeb3e51b835f
user <joeycli0919@gmail.com>
2022-01-27 13:59:20 +0800

    add lastheader param

--- cmd/devtrie/trieinspector.go
+++ cmd/devtrie/trieinspector.go
@@ -18,31 +18,36 @@ var trieInspectorCommand = cli.Command{
 	Flags: []cli.Flag{
 		cli.StringFlag{Name: "path"},
 		cli.Uint64Flag{Name: "num"},
+		cli.BoolFlag{Name: "lastheader"},
 	},
 }
 
 func trieInspectorRun(ctx *cli.Context) error {
-	path := ctx.String("path")
-	num  := ctx.Uint64("num")
-	if num <= 0 {
-		fmt.Println("num invalid")
-		return nil
-	}
+	path 		:= ctx.String("path")
+	num  		:= ctx.Uint64("num")
+	lastheader  := ctx.Bool("lastheader")
+
 
     db, err := rawdb.NewLevelDBDatabaseWithFreezer(path, 512, 20000, filepath.Join(path, "ancient"), "eth/db/chaindata/", true)
     if err != nil {
     	return err
     }
 
-    hash := rawdb.ReadCanonicalHash(db, num)
-	if hash == (common.Hash{}) {
-		fmt.Println("the num of header is empty")
-		return nil
-		
-	}
-	header := rawdb.ReadHeader(db, hash, num)
+    var rootHash common.Hash
+    if lastheader {
+    	rootHash = rawdb.ReadHeadHeaderHash(db)
+    } else {
+    	hash := rawdb.ReadCanonicalHash(db, num)
+		if hash == (common.Hash{}) {
+			fmt.Println("the num of header is empty")
+			return nil
+			
+		}
+		header := rawdb.ReadHeader(db, hash, num)
+		rootHash = header.Root
+    }
 
-    tr, err := trie.New(header.Root, trie.NewDatabase(db))
+    tr, err := trie.New(rootHash, trie.NewDatabase(db))
     if err != nil {
     	return err
     }
